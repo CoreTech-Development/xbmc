30c534acf92bb9952712145cdef3dda52ec97aa9
croniccorey <cronmod.dev@gmail.com>
2014-10-17 05:57:13 -0400

    ActiveAESink/AMLUtils: Force passthrough on all SoCs
    Conflicts:
    	xbmc/utils/AMLUtils.cpp

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAESink.cpp
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAESink.cpp
@@ -32,6 +32,10 @@
 #include <new> // for std::bad_alloc
 #include <algorithm>
 
+#if defined(HAS_LIBAMCODEC)
+#include "utils/AMLUtils.h"
+#endif
+
 using namespace ActiveAE;
 
 CActiveAESink::CActiveAESink(CEvent *inMsgEvent) :
@@ -93,6 +97,11 @@ AEDeviceType CActiveAESink::GetDeviceType(const std::string &device)
 
 bool CActiveAESink::HasPassthroughDevice()
 {
+#if defined(HAS_LIBAMCODEC)
+  if (aml_get_device_type() != AML_DEVICE_TYPE_UNKNOWN
+    && aml_get_device_type() >= AML_DEVICE_TYPE_M1)
+    return true;
+#endif
   for (AESinkInfoList::iterator itt = m_sinkInfoList.begin(); itt != m_sinkInfoList.end(); ++itt)
   {
     for (AEDeviceInfoList::iterator itt2 = itt->m_deviceInfoList.begin(); itt2 != itt->m_deviceInfoList.end(); ++itt2)
@@ -107,6 +116,11 @@ bool CActiveAESink::HasPassthroughDevice()
 
 bool CActiveAESink::SupportsFormat(const std::string &device, AEDataFormat format, int samplerate)
 {
+#if defined(HAS_LIBAMCODEC)
+  if (aml_get_device_type() != AML_DEVICE_TYPE_UNKNOWN
+    && aml_get_device_type() >= AML_DEVICE_TYPE_M1)
+    return true;
+#endif
   std::string dev = device;
   std::string dri;
   CAESinkFactory::ParseDevice(dev, dri);
