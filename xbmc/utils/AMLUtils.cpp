09163a89dc828f9743abdcc71cbee86c4145d422
croniccorey <cronmod.dev@gmail.com>
2014-11-23 19:46:54 -0500

    [AMLUtils] Add support for <=Meson6 on Android 4.4.x
    Conflicts:
    	xbmc/utils/AMLUtils.cpp

--- xbmc/utils/AMLUtils.cpp
+++ xbmc/utils/AMLUtils.cpp
@@ -367,8 +367,15 @@ bool aml_mode_to_resolution(const char *mode, RESOLUTION_INFO *res)
   }
   else if (StringUtils::EqualsNoCase(fromMode, "1080p"))
   {
-    res->iWidth = 1920;
-    res->iHeight= 1080;
+#if defined(TARGET_ANDROID)
+    if (aml_get_device_type() <= AML_DEVICE_TYPE_M6) {
+      res->iWidth = 1280;
+      res->iHeight= 720;
+    } else {
+      res->iWidth = 1920;
+      res->iHeight= 1080;
+    }
+#endif
     res->iScreenWidth = 1920;
     res->iScreenHeight= 1080;
     res->fRefreshRate = 60;
@@ -376,8 +383,15 @@ bool aml_mode_to_resolution(const char *mode, RESOLUTION_INFO *res)
   }
   else if (StringUtils::EqualsNoCase(fromMode, "1080p24hz"))
   {
-    res->iWidth = 1920;
-    res->iHeight= 1080;
+#if defined(TARGET_ANDROID)
+    if (aml_get_device_type() <= AML_DEVICE_TYPE_M6) {
+      res->iWidth = 1280;
+      res->iHeight= 720;
+    } else {
+      res->iWidth = 1920;
+      res->iHeight= 1080;
+    }
+#endif
     res->iScreenWidth = 1920;
     res->iScreenHeight= 1080;
     res->fRefreshRate = 24;
@@ -385,8 +399,15 @@ bool aml_mode_to_resolution(const char *mode, RESOLUTION_INFO *res)
   }
   else if (StringUtils::EqualsNoCase(fromMode, "1080p30hz"))
   {
-    res->iWidth = 1920;
-    res->iHeight= 1080;
+#if defined(TARGET_ANDROID)
+    if (aml_get_device_type() <= AML_DEVICE_TYPE_M6) {
+      res->iWidth = 1280;
+      res->iHeight= 720;
+    } else {
+      res->iWidth = 1920;
+      res->iHeight= 1080;
+    }
+#endif
     res->iScreenWidth = 1920;
     res->iScreenHeight= 1080;
     res->fRefreshRate = 30;
@@ -394,8 +415,15 @@ bool aml_mode_to_resolution(const char *mode, RESOLUTION_INFO *res)
   }
   else if (StringUtils::EqualsNoCase(fromMode, "1080p50hz"))
   {
-    res->iWidth = 1920;
-    res->iHeight= 1080;
+#if defined(TARGET_ANDROID)
+    if (aml_get_device_type() <= AML_DEVICE_TYPE_M6) {
+      res->iWidth = 1280;
+      res->iHeight= 720;
+    } else {
+      res->iWidth = 1920;
+      res->iHeight= 1080;
+    }
+#endif
     res->iScreenWidth = 1920;
     res->iScreenHeight= 1080;
     res->fRefreshRate = 50;
@@ -403,8 +431,15 @@ bool aml_mode_to_resolution(const char *mode, RESOLUTION_INFO *res)
   }
   else if (StringUtils::EqualsNoCase(fromMode, "1080i"))
   {
-    res->iWidth = 1920;
-    res->iHeight= 1080;
+#if defined(TARGET_ANDROID)
+    if (aml_get_device_type() <= AML_DEVICE_TYPE_M6) {
+      res->iWidth = 1280;
+      res->iHeight= 720;
+    } else {
+      res->iWidth = 1920;
+      res->iHeight= 1080;
+    }
+#endif
     res->iScreenWidth = 1920;
     res->iScreenHeight= 1080;
     res->fRefreshRate = 60;
@@ -412,8 +447,15 @@ bool aml_mode_to_resolution(const char *mode, RESOLUTION_INFO *res)
   }
   else if (StringUtils::EqualsNoCase(fromMode, "1080i50hz"))
   {
-    res->iWidth = 1920;
-    res->iHeight= 1080;
+#if defined(TARGET_ANDROID)
+    if (aml_get_device_type() <= AML_DEVICE_TYPE_M6) {
+      res->iWidth = 1280;
+      res->iHeight= 720;
+    } else {
+      res->iWidth = 1920;
+      res->iHeight= 1080;
+    }
+#endif
     res->iScreenWidth = 1920;
     res->iScreenHeight= 1080;
     res->fRefreshRate = 50;
