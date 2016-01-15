971348d3f5a631b47b35c36788546e62f81214bb
croniccorey <cronmod.dev@gmail.com>
2016-01-15 16:23:44 -0500

    SystemInfo: Changes to GetVersion()

--- xbmc/utils/SystemInfo.cpp
+++ xbmc/utils/SystemInfo.cpp
@@ -1249,7 +1249,7 @@ std::string CSysInfo::GetVersionShort()
 
 std::string CSysInfo::GetVersion()
 {
-  return GetVersionShort() + " Git:" + CCompileInfo::GetSCMID();
+  return GetVersionShort() + "~" + CCompileInfo::GetSCMID();
 }
 
 std::string CSysInfo::GetBuildDate()
